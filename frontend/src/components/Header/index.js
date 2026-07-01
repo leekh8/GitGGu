bbff46b722386076dd81a6571687bd4598c47422
leekh8 <amysun125@gmail.com>
2026-07-01 14:22:14 +0900

    Fix: use default import for Home.svg URL

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { Home } from "../../assets/Home.svg";
+import Home from "../../assets/Home.svg";
 
 import Logo from "../Logo";
 
